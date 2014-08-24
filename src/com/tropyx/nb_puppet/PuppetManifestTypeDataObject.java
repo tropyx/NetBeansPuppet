3d545fea089b2f447750f7eb1c5596b6fb30f2d0
mkleint@gmail.com <>
2014-08-24 20:28:08 +1000

    editor tab contains module name for easier orientation

--- src/com/tropyx/nb_puppet/PuppetManifestTypeDataObject.java
+++ src/com/tropyx/nb_puppet/PuppetManifestTypeDataObject.java
@@ -18,19 +18,35 @@
 package com.tropyx.nb_puppet;
 
 import java.io.IOException;
+import org.netbeans.core.api.multiview.MultiViews;
 import org.netbeans.core.spi.multiview.MultiViewElement;
 import org.netbeans.core.spi.multiview.text.MultiViewEditorElement;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
+import org.openide.cookies.CloseCookie;
+import org.openide.cookies.EditCookie;
+import org.openide.cookies.EditorCookie;
+import org.openide.cookies.OpenCookie;
+import org.openide.cookies.PrintCookie;
+import org.openide.cookies.SaveCookie;
+import org.openide.filesystems.FileChangeAdapter;
+import org.openide.filesystems.FileChangeListener;
+import org.openide.filesystems.FileEvent;
+import org.openide.filesystems.FileLock;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.openide.filesystems.MIMEResolver;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiDataObject;
 import org.openide.loaders.MultiFileLoader;
+import org.openide.nodes.CookieSet;
+import org.openide.text.CloneableEditorSupport;
+import org.openide.text.DataEditorSupport;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle.Messages;
+import org.openide.windows.CloneableOpenSupport;
 import org.openide.windows.TopComponent;
 
 @Messages({
@@ -101,7 +117,9 @@ public class PuppetManifestTypeDataObject extends MultiDataObject {
 
     public PuppetManifestTypeDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
         super(pf, loader);
-        registerEditor("text/x-puppet-manifest", true);
+//        registerEditor("text/x-puppet-manifest", true);
+        CookieSet cookies = getCookieSet();
+        cookies.add(new PPManifestDataEditor());
     }
 
     @Override
@@ -120,4 +138,125 @@ public class PuppetManifestTypeDataObject extends MultiDataObject {
     public static MultiViewEditorElement createEditor(Lookup lkp) {
         return new MultiViewEditorElement(lkp);
     }
+    
+    private static class AtlPlgEnv extends DataEditorSupport.Env {
+
+        private static final long serialVersionUID = 1L;
+
+        AtlPlgEnv(MultiDataObject d) {
+            super(d);
+        }
+
+        protected @Override
+        FileObject getFile() {
+            return getDataObject().getPrimaryFile();
+        }
+
+        protected @Override
+        FileLock takeLock() throws IOException {
+            return ((MultiDataObject) getDataObject()).getPrimaryEntry().takeLock();
+        }
+
+        public @Override
+        CloneableOpenSupport findCloneableOpenSupport() {
+            return getDataObject().getLookup().lookup(PPManifestDataEditor.class);
+        }
+
+    }
+    private class PPManifestDataEditor extends DataEditorSupport implements EditorCookie.Observable, OpenCookie, EditCookie, PrintCookie, CloseCookie {
+
+        private String cachedTitleSuffix = null;
+        private final Object TITLE_LOCK = new Object();
+        private final SaveCookie save = new SaveCookie() {
+            public @Override void save() throws IOException {
+                saveDocument();
+//                PuppetManifestTypeDataObject.this.resetDocument();
+            }
+            @Override public String toString() {
+                return getPrimaryFile().getNameExt();
+            }
+        };
+
+        private final FileChangeListener listener = new FileChangeAdapter() {
+            public @Override void fileChanged(FileEvent fe) {
+                synchronized (TITLE_LOCK) {
+                    cachedTitleSuffix = null;
+                }
+                updateTitles();
+            }
+        };
+
+        PPManifestDataEditor() {
+            super(PuppetManifestTypeDataObject.this, null, new AtlPlgEnv(PuppetManifestTypeDataObject.this));
+            getPrimaryFile().addFileChangeListener(FileUtil.weakFileChangeListener(listener, getPrimaryFile()));
+        }
+
+        @Override
+        protected CloneableEditorSupport.Pane createPane() {
+            return (CloneableEditorSupport.Pane) MultiViews.createCloneableMultiView("text/x-puppet-manifest", getDataObject());
+        }
+
+        protected @Override
+        boolean notifyModified() {
+            if (!super.notifyModified()) {
+                return false;
+            }
+            if (getLookup().lookup(SaveCookie.class) == null) {
+                getCookieSet().add(save);
+                setModified(true);
+            }
+            return true;
+        }
+
+        protected @Override
+        void notifyUnmodified() {
+            super.notifyUnmodified();
+            if (getLookup().lookup(SaveCookie.class) == save) {
+                getCookieSet().remove(save);
+                setModified(false);
+            }
+        }
+
+        protected @Override
+        String messageName() {
+            String titleSuffix = null;
+            synchronized (TITLE_LOCK) {
+                if (cachedTitleSuffix == null) {
+                    cachedTitleSuffix = annotateWithKey(getPrimaryFile());
+                }
+                titleSuffix = cachedTitleSuffix;
+            }
+            return super.messageName() + titleSuffix;
+        }
+
+        protected @Override
+        String messageHtmlName() {
+            String titleSuffix = null;
+            synchronized (TITLE_LOCK) {
+                if (cachedTitleSuffix == null) {
+                    cachedTitleSuffix = annotateWithKey(getPrimaryFile());
+                }
+                titleSuffix = cachedTitleSuffix;
+            }
+            return super.messageHtmlName() + titleSuffix;
+        }
+
+        protected @Override
+        boolean asynchronousOpen() {
+            return true;
+        }
+
+    }
+
+    static String annotateWithKey(FileObject primaryFile) {
+        final FileObject parent = primaryFile.getParent();
+        if (parent != null && "manifests".equals(parent.getNameExt())) {
+            FileObject pp = parent.getParent();
+            if (pp != null) {
+                return " [" + pp.getNameExt() + "]";
+            }
+        }
+        return "";
+    }
+
 }
