b9956c00ad8940f9a67f2930ac53c3405e14bc44
mkleint@gmail.com <>
2014-08-25 21:46:51 +1000

    UI and simplistic backend to configure what lint checks should be disabled (yes, 80 char line drives me nuts)

---
+++ src/com/tropyx/nb_puppet/lint/LintCustomizerPanel.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 mkleint
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.tropyx.nb_puppet.lint;
+
+import javax.swing.JComponent;
+import org.netbeans.api.project.Project;
+import org.netbeans.spi.project.ui.support.ProjectCustomizer;
+import org.openide.util.Lookup;
+
+@ProjectCustomizer.CompositeCategoryProvider.Registration(projectType = "com-tropyx-nb_puppet", position = 100)
+public class LintCustomizerPanel implements ProjectCustomizer.CompositeCategoryProvider{
+
+    @Override
+    public ProjectCustomizer.Category createCategory(Lookup context) {
+        return ProjectCustomizer.Category.create("Lint", "Puppet Lint", null);
+    }
+
+    @Override
+    public JComponent createComponent(ProjectCustomizer.Category category, Lookup context) {
+        return new LintPanelUI(category, context.lookup(Project.class));
+    }
+
+}
--- src/com/tropyx/nb_puppet/lint/StatusProvider.java
+++ src/com/tropyx/nb_puppet/lint/StatusProvider.java
@@ -14,6 +14,8 @@ import java.util.prefs.Preferences;
 import javax.swing.text.Document;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
 import org.netbeans.api.extexecution.ExternalProcessBuilder;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
 import org.netbeans.modules.editor.NbEditorUtilities;
 import org.netbeans.spi.editor.errorstripe.UpToDateStatus;
 import org.netbeans.spi.editor.errorstripe.UpToDateStatusProvider;
@@ -22,6 +24,7 @@ import org.netbeans.spi.editor.hints.ErrorDescription;
 import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
 import org.netbeans.spi.editor.hints.HintsController;
 import org.netbeans.spi.editor.hints.Severity;
+import org.netbeans.spi.project.AuxiliaryProperties;
 import org.openide.filesystems.FileChangeAdapter;
 import org.openide.filesystems.FileEvent;
 import org.openide.filesystems.FileObject;
@@ -50,10 +53,12 @@ public final class StatusProvider implements UpToDateStatusProviderFactory {
         private final Document document;
         private final AtomicBoolean dirty = new AtomicBoolean(true);
         private final AtomicBoolean running = new AtomicBoolean(false);
+        private final Project project;
 
         StatusProviderImpl(Document doc) {
             this.document = doc;
             FileObject fo = NbEditorUtilities.getFileObject(document);
+            project = FileOwnerQuery.getOwner(fo);
             fo.addFileChangeListener(new FileChangeAdapter() {
 
                 @Override
@@ -140,9 +145,17 @@ public final class StatusProvider implements UpToDateStatusProviderFactory {
 
         private ExternalProcessBuilder skipChecks(ExternalProcessBuilder builder)
         {
+            //global prefs
             Preferences nd = NbPreferences.forModule(StatusProvider.class).node("lint");
+            AuxiliaryProperties p = null;
+            if (project != null) {
+                p = project.getLookup().lookup(AuxiliaryProperties.class);
+            }
             for (LintCheck lc : LintCheck.values()) {
-                if (!nd.getBoolean(lc.name(), true)) {
+                if (p != null && "false".equals(p.get("lint." + lc.name(), true))) {
+                    builder = builder.addArgument(lc.getDisableParam());
+                }
+                else if (!nd.getBoolean(lc.name(), true)) {
                     builder = builder.addArgument(lc.getDisableParam());
                 }
             }
