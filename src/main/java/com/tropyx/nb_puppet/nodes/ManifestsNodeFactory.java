2be5d2b753c071856bf3bd5737b87bdd15a553f6
Milos Kleint <mkleint@gmail.com>
2015-09-06 21:55:58 +1000

    navigator for puppet files showing all neighbouring .pp files (same module)

--- src/main/java/com/tropyx/nb_puppet/nodes/ManifestsNodeFactory.java
+++ src/main/java/com/tropyx/nb_puppet/nodes/ManifestsNodeFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 github.com/tropyx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.tropyx.nb_puppet.nodes;
 
 import java.util.ArrayList;
@@ -35,7 +51,7 @@ public class ManifestsNodeFactory implements NodeFactory {
         @Override
         public List<Node> keys() {
             FileObject manifestsFolder = project.getProjectDirectory().getFileObject("manifests");
-            List<Node> result = new ArrayList<Node>();
+            List<Node> result = new ArrayList<>();
             if (manifestsFolder != null) {
                 for (FileObject manifestsFolderFile : manifestsFolder.getChildren()) {
                     try {
